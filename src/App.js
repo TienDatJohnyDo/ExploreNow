b621cedc40eb5f23cbcd28609b87f6b960b3a21f
Johny Do <102762223+TienDatJohnyDo@users.noreply.github.com>
2024-03-19 21:21:52 -0600

    First draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react'; 
 import './App.css';
+import logoImage from './logo.png'
+
 
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Centered rectangle container */}
+        <div className="centered-rectangle">
+          <img src={logoImage} className="App-logo" alt="logo" />
+          <p> Travel. Explore. Make Memories </p>
+        </div>
+        <div className = "blue-centered-rectangle">
+          <h1> Welcome!</h1>
+          <p> Let's get Started!</p>
+          <p>
+            I agree to Explore Now's{' '}
+            <a href="/terms-of-service" target="_blank" rel="noopener noreferrer">
+              Term of Service <br />
+            </a>{' '}
+            and confirm that I have read Explore Now's{' '} <br />
+            <a href="/privacy-policy" target="_blank" rel="noopener noreferrer">
+              Privacy Policy
+            </a>
+            .
+            <br />
+            <br />
+          </p>
+          {/* Two Buttons for Login and New guest */}
+          <div className="button-container">
+            
+            <button>Login</button>
+          
+            
+            <button>I'm New</button>
+            
+          </div>
+          <p> Or</p>
+          <div className="button-container">
+            <button>Guest User</button>
+          </div>
+        </div>
+
       </header>
     </div>
+
   );
 }
-
 export default App;
