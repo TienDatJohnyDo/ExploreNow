86ae7cf248756cb810d7edd07b056a3314767577
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2024-03-20 14:56:28 -0600

    added mongoose connection to db

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const testRoutes = require('./routes/testRoute')
+
+const MONGO_URL = "mongodb+srv://test:testPassword@explorenowdb.gmrwrda.mongodb.net/?retryWrites=true&w=majority&appName=ExploreNowDB"
 
 //create express app
 const app = express()
@@ -10,10 +13,18 @@ app.get('/', (req, res) => {
 
 app.use('/api/test',testRoutes)
 
+mongoose.connect(MONGO_URL)
+    .then(() => {
+        app.listen(4000, () => {
+            console.log("Listening on port 4000")
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
 //listen for requests
-app.listen(4000, () => {
-    console.log("Listening on port 4000")
-})
+
 
 
 
