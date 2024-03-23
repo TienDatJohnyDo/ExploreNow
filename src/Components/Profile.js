4166452a07f08e7aed544125323a39331ea1c04a
Johny Do <102762223+TienDatJohnyDo@users.noreply.github.com>
2024-03-23 15:19:51 -0600

    Profile added, with stats for the Lauren

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './styles/search.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'; // Importing the "more" icon
+import lauren from '../lauren.jpeg'
 
 function Profile() {
   return (
     <div className="discover-container">
-      <h1 className="discover-label">Profile </h1>
+      <div className="profile-info">
+        <div className="profile-picture-container">
+          <img src={lauren} alt="Profile" className="profile-picture" />
+        </div>
+        <div className="profile-text">
+          <h2>Lauren Cromwell</h2>
+          <p>Travel Blogger <br />Collaborations open!</p>
+        </div>
+      </div>
+      <div className="profile-stats-container">
+        <div className="profile-stat">
+          <p className="stat-number">2</p>
+          <p className="stat-category">Posts</p>
+        </div>
+        <div className="profile-stat">
+          <p className="stat-number">1.1 k</p>
+          <p className="stat-category">Followers</p>
+        </div>
+        <div className="profile-stat">
+          <p className="stat-number">500</p>
+          <p className="stat-category">Following</p>
+        </div>
+      </div>
+
+
+      {/* Add in at least 2 Post  */}
+            
+      <FontAwesomeIcon icon={faEllipsisV} className="more-icon" />
     </div>
   );
 }
