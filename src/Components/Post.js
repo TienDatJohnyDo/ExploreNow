01421d77d83483ae2e3d5506729668cbd1283c38
Johny Do <102762223+TienDatJohnyDo@users.noreply.github.com>
2024-03-25 18:12:44 -0600

    Started on Post page

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -3,25 +3,23 @@ import './styles/post.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
-
 function Post() {
   return (
     <div className="discover-container">
-      <h1 className="discover-label">POST</h1>
-
-      <h2> Hey Lauren! </h2> 
-      <h2> Share Your Journey</h2>
-      <h3> Where did you go? </h3>
-      
-      <div className="text-input-container">
+      <div className="top-section">
+        <h1 className="discover-label">POST</h1> 
         
-        <input 
-          type="text"
-          placeholder="Add Location"
-          className="text-input"
-        />
-        <FontAwesomeIcon icon={faSearch} color="grey" size="lg" style={{ marginLeft: '-30px', zIndex: '1' }} />
       </div>
+
+      <div className="text-input-container">
+          <input 
+            type="text"
+            placeholder="Add Location"
+            className="text-input"
+          />
+          <FontAwesomeIcon icon={faSearch} color="grey" size="lg" style={{ marginLeft: '-30px', zIndex: '1' }} />
+        </div>
+      {/* Other content of your component */}
     </div>
   );
 }
